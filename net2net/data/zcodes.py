e1c8d9c16f279025dd34993a8d1c6b8f6b3675dc
Patrick Esser <Patrick.Esser@gmx.net>
2020-12-09 14:26:11 +0100

    fix prng in dataloader

--- net2net/data/zcodes.py
+++ net2net/data/zcodes.py
@@ -1,16 +1,30 @@
+import os
 import numpy as np
 from tqdm import tqdm
 from torch.utils.data import Dataset
 
 
-class TrainSamples(Dataset):
-    def __init__(self, n_samples, z_shape, n_classes, truncation=-1):
-        self.prng = np.random.RandomState(1)
+class PRNGMixin(object):
+    """Adds a prng property which is a numpy RandomState which gets
+    reinitialized whenever the pid changes to avoid synchronized sampling
+    behavior when used in conjunction with multiprocessing."""
+
+    @property
+    def prng(self):
+        currentpid = os.getpid()
+        if getattr(self, "_initpid", None) != currentpid:
+            self._initpid = currentpid
+            self._prng = np.random.RandomState()
+        return self._prng
+
+
+class TrainSamples(Dataset, PRNGMixin):
+    def __init__(self, n_samples, z_shape, n_classes, truncation=0):
         self.n_samples = n_samples
         self.z_shape = z_shape
         self.n_classes = n_classes
         self.truncation_threshold = truncation
-        if self.truncation_threshold > -1:
+        if self.truncation_threshold > 0:
             print("Applying truncation at level {}".format(self.truncation_threshold))
 
     def __len__(self):
@@ -18,7 +32,7 @@ class TrainSamples(Dataset):
 
     def __getitem__(self, i):
         z = self.prng.randn(*self.z_shape)
-        if self.truncation_threshold > -1:
+        if self.truncation_threshold > 0:
             for k, zi in enumerate(z):
                 while abs(zi) > self.truncation_threshold:
                     zi = self.prng.randn(1)
@@ -28,16 +42,16 @@ class TrainSamples(Dataset):
 
 
 class TestSamples(Dataset):
-    def __init__(self, n_samples, z_shape, n_classes, truncation=-1):
+    def __init__(self, n_samples, z_shape, n_classes, truncation=0):
         self.prng = np.random.RandomState(1)
         self.n_samples = n_samples
         self.z_shape = z_shape
         self.n_classes = n_classes
         self.truncation_threshold = truncation
-        if self.truncation_threshold > -1:
+        if self.truncation_threshold > 0:
             print("Applying truncation at level {}".format(self.truncation_threshold))
         self.zs = self.prng.randn(self.n_samples, *self.z_shape)
-        if self.truncation_threshold > -1:
+        if self.truncation_threshold > 0:
             print("Applying truncation at level {}".format(self.truncation_threshold))
             ix = 0
             for z in tqdm(self.zs, desc="Truncation:"):
@@ -57,15 +71,14 @@ class TestSamples(Dataset):
         return {"z": self.zs[i].astype(np.float32), "class": self.clss[i]}
 
 
-class RestrictedTrainSamples(Dataset):
-    def __init__(self, n_samples, z_shape, truncation=-1):
+class RestrictedTrainSamples(Dataset, PRNGMixin):
+    def __init__(self, n_samples, z_shape, truncation=0):
         index_path = "data/coco_imagenet_overlap_idx.txt"
-        self.prng = np.random.RandomState(1)
         self.n_samples = n_samples
         self.z_shape = z_shape
         self.classes = np.loadtxt(index_path).astype(int)
         self.truncation_threshold = truncation
-        if self.truncation_threshold > -1:
+        if self.truncation_threshold > 0:
             print("Applying truncation at level {}".format(self.truncation_threshold))
 
     def __len__(self):
@@ -73,7 +86,7 @@ class RestrictedTrainSamples(Dataset):
 
     def __getitem__(self, i):
         z = self.prng.randn(*self.z_shape)
-        if self.truncation_threshold > -1:
+        if self.truncation_threshold > 0:
             for k, zi in enumerate(z):
                 while abs(zi) > self.truncation_threshold:
                     zi = self.prng.randn(1)
@@ -83,7 +96,7 @@ class RestrictedTrainSamples(Dataset):
 
 
 class RestrictedTestSamples(Dataset):
-    def __init__(self, n_samples, z_shape, truncation=-1):
+    def __init__(self, n_samples, z_shape, truncation=0):
         index_path = "data/coco_imagenet_overlap_idx.txt"
 
         self.prng = np.random.RandomState(1)
@@ -94,7 +107,7 @@ class RestrictedTestSamples(Dataset):
         self.clss = self.prng.choice(self.classes, size=(self.n_samples,), replace=True)
         self.truncation_threshold = truncation
         self.zs = self.prng.randn(self.n_samples, *self.z_shape)
-        if self.truncation_threshold > -1:
+        if self.truncation_threshold > 0:
             print("Applying truncation at level {}".format(self.truncation_threshold))
             ix = 0
             for z in tqdm(self.zs, desc="Truncation:"):
